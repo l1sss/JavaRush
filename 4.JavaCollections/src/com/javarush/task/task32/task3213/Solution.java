bbd5b24ed3ffc983e75016cf83206886b10dbbd9
Dima Slisenko <dm.slisenko@gmail.com>
2017-10-19 12:17:00 +0300

    Caesar's code

--- 4.JavaCollections/src/com/javarush/task/task32/task3213/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3213/Solution.java
@@ -1,6 +1,5 @@
 package com.javarush.task.task32.task3213;
 
-import java.io.IOException;
 import java.io.StringReader;
 
 /* 
@@ -8,13 +7,13 @@ import java.io.StringReader;
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         StringReader reader = new StringReader("Khoor Dpljr");
         System.out.println(decode(reader, -3));  //Hello Amigo
 
     }
 
-    public static String decode(StringReader reader, int key) throws IOException {
+    public static String decode(StringReader reader, int key) {
         StringBuilder sb = new StringBuilder();
 
         try {
