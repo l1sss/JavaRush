3698194dbc152c3968464102d53aa0bff20d2221
Dima Slisenko <dm.slisenko@gmail.com>
2017-10-27 12:13:12 +0300

    XML deserialization

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3308/Shop.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task33.task3308;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@XmlRootElement
+@XmlType(name = "shop")
+public class Shop {
+    public Goods goods;
+    public int count;
+    public double profit;
+    public String[] secretData;
+
+    public static class Goods {
+        public List<String> names = new ArrayList<>();
+
+        @Override
+        public String toString() {
+            return "Goods{" +
+                    "names=" + names +
+                    '}';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Shop{" +
+                "goods=" + goods +
+                ", count=" + count +
+                ", profit=" + profit +
+                ", secretData=" + Arrays.toString(secretData) +
+                '}';
+    }
+}
