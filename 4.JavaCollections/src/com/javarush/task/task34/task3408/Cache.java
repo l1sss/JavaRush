3ec8815c515245f9c9cfa0e5b49f11d6f7d70af4
Dima Slisenko <dm.slisenko@gmail.com>
2017-11-08 12:50:01 +0300

    Cache Reflection

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3408/Cache.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task34.task3408;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class Cache<K, V> {
+    private Map<K, V> cache = new WeakHashMap<>();
+
+    public V getByKey(K key, Class<V> clazz) throws Exception {
+        if (!cache.containsKey(key)) cache.put(key, clazz.getConstructor(key.getClass()).newInstance(key));
+
+        return cache.get(key);
+    }
+
+    @SuppressWarnings("unchecked")
+    public boolean put(V obj) {
+        Method method;
+        try {
+            method = obj.getClass().getDeclaredMethod("getKey");
+            method.setAccessible(true);
+
+            K key = (K) method.invoke(obj);
+            cache.put(key, obj);
+
+            return cache.containsKey(key);
+        } catch (Exception e) {
+            //ignore
+        }
+        return false;
+    }
+
+    public int size() {
+        return cache.size();
+    }
+}
