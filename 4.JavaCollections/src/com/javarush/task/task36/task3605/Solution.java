4fda107047fba316877500d0b93b7fda415330b4
Dima Slisenko <dm.slisenko@gmail.com>
2017-11-30 17:23:20 +0300

    use TreeSet

--- 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
@@ -14,17 +14,15 @@ import java.util.TreeSet;
 public class Solution {
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
-
         Path path = Paths.get(args[0]);
         sb.append(Files.readAllLines(path));
 
         String text = sb.toString().toLowerCase().replaceAll("[^a-z]", "");
-
         TreeSet<Character> set = new TreeSet<>();
         for (char c : text.toCharArray())
             set.add(c);
-        List<Character> list = new ArrayList<>(set);
 
+        List<Character> list = new ArrayList<>(set);
         int printSize = list.size() < 5 ? list.size() : 5;
         for (int i = 0; i < printSize; i++) {
             System.out.print(list.get(i));
