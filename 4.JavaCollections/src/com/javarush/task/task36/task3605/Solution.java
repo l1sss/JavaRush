6a355897b938dbe4b3d2244d34c4e55762a34151
Dima Slisenko <dm.slisenko@gmail.com>
2017-11-30 17:21:04 +0300

    use TreeSet

--- 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
@@ -1,7 +1,5 @@
 package com.javarush.task.task36.task3605;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,25 +13,19 @@ import java.util.TreeSet;
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
-        List<Character> list = null;
+        StringBuilder sb = new StringBuilder();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
-            StringBuilder sb = new StringBuilder();
+        Path path = Paths.get(args[0]);
+        sb.append(Files.readAllLines(path));
 
-            Path path = Paths.get(args[0]);
-            sb.append(Files.readAllLines(path));
+        String text = sb.toString().toLowerCase().replaceAll("[^a-z]", "");
 
-            String text = sb.toString().toLowerCase().replaceAll("[^a-z]", "");
-
-            TreeSet<Character> set = new TreeSet<>();
-            for (char c : text.toCharArray())
-                set.add(c);
-
-            list = new ArrayList<>(set);
-        }
+        TreeSet<Character> set = new TreeSet<>();
+        for (char c : text.toCharArray())
+            set.add(c);
+        List<Character> list = new ArrayList<>(set);
 
         int printSize = list.size() < 5 ? list.size() : 5;
-
         for (int i = 0; i < printSize; i++) {
             System.out.print(list.get(i));
         }
